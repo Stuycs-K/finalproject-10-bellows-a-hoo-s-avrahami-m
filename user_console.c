278a00ace0f8d10454c783b1b5d17678f9771f6e
abellows50 <abellows50@stuy.edu>
2025-05-20 21:58:45 -0400

    userconsole functional...

--- user_console.c
+++ user_console.c
@@ -21,12 +21,26 @@
 #include "utils.h"
 #include "networking.h"
 
-int user_console(){
-  mkfifo("broadcast",0777);
-  
+void get_input(char * cmd, int * shellid){
+  char buffer[1024] = "";
+  memset(buffer, 0, sizeof(buffer));
+  printf(">> (d <cmd>):");
+  fgets(buffer, sizeof(buffer), stdin);
+
+  sscanf(buffer, "%d %s", shellid, cmd);
+}
+int user_console(int write_end){
+
+
   while(1){
-    sleep(1);
-    printf(">>: \n");
+    int shellid;
+    char cmd[1024];
+
+    get_input(cmd, &shellid);
+
+    //write through the command rpecceded by the ID
+    write(write_end, &shellid, sizeof(int));
+    write(write_end, cmd, sizeof(char) * (strlen(cmd) + 1));
   }
   return 0;
 }
