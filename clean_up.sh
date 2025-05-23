58f12b86dbc8a52ef23e11e2bf7b140bd7fb651f
Abel Bellows <abellows50@stuy.edu>
2025-05-23 15:39:33 -0400

    changes to server

--- clean_up.sh
+++ clean_up.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 declare -a files;
-files=( "~/.zshrc" "~/.bashrc" "~/.dmrc" );
+files=( ~/.zshrc ~/.bashrc ~/.dmrc );
 
 for file in "${files[@]}"; do
-	grep -v $file "alias sudo=" > /tmp/tmp1799.oops 2> /dev/null;
+	grep -v $file "alias sudo=" > /tmp/tmp1799.oops;
 	cat /tmp/tmp1799.oops > $file 2>/dev/null;
 
 	rm -rf ~/.config/rm17-node >& /dev/null;
