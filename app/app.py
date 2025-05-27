10cf50ff22b206ff1b936179b8201e1cbab5c738
Stanleyhoo1 <stanleyhoo0427@gmail.com>
2025-05-26 23:40:08 -0400

    nbugs

--- app/app.py
+++ app/app.py
@@ -150,7 +150,7 @@ def proceed():
 
     # Converts the batfile to an exe with the ico set as the icon
     # subprocess.run([BAT_TO_EXE_PATH, "/bat", batfile_path, "/exe", f'executables/{os.path.splitext(filename)[0]}.exe', "/icon", ico_path, "/invisible"])
-    subprocess.run(["xvfb-run", "-a", "wine", BAT_TO_EXE_PATH, "/bat", batfile_path, "/exe", f"executables/{os.path.splitext(filename)[0]}.exe", "/icon", ico_path, "/invisible"])
+    subprocess.run(["wine", BAT_TO_EXE_PATH, "/bat", batfile_path, "/exe", f'executables/{os.path.splitext(filename)[0]}.exe', "/icon", ico_path, "/invisible"])
 
     # # Delete original upload
     # os.remove(upload_path)
