2d0bce1e000e173ac6aa6751249eb0f6ac81ad2d
Stanleyhoo1 <stanleyhoo0427@gmail.com>
2025-05-30 01:00:50 -0400

    bug fix

--- app/app.py
+++ app/app.py
@@ -6,6 +6,7 @@ import secrets
 import subprocess
 import uuid
 import time
+import shutil
 from PIL import Image
 
 app = Flask(__name__)
@@ -172,7 +173,7 @@ def proceed():
     
     # Removes image background
     subprocess.run(["backgroundremover", "-i", upload_path, "-o", processed_path])
-    #subprocess.run(["cp", processed_path, png_path])
+    shutil.copy(processed_path, png_path)
     
     # Creates the ico from the image
     img = Image.open(processed_path)
